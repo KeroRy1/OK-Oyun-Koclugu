71796c139b05d702dfc58c52dd3f9dffd2074c3e
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-06 11:32:09 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,24 +6,18 @@ class Coach(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     game = db.Column(db.String(50))
-    level = db.Column(db.String(50))  # Basit, Orta, Pro, Oyun Ustası
+    level = db.Column(db.String(50))
     zoom_email = db.Column(db.String(100))
-    rating = db.Column(db.Float, default=0.0)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer)
-    coach_id = db.Column(db.Integer, db.ForeignKey("coach.id"))
+    coach_id = db.Column(db.Integer)
     price = db.Column(db.Float)
     session_id = db.Column(db.String(100))
 
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    coach_id = db.Column(db.Integer, db.ForeignKey("coach.id"))
+    coach_id = db.Column(db.Integer)
     rating = db.Column(db.Integer)
     comment = db.Column(db.Text)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    is_admin = db.Column(db.Boolean, default=False)

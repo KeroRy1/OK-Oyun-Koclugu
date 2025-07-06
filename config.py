d6abdc2a2854cc5d16dfc64a17120da04c8787b3
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-06 12:17:27 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "gizli-anahtar"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "sk_test_...")

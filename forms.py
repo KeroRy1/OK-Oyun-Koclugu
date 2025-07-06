aba6cedf5d38070629fff288f5a4302c2c0867f5
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-06 11:32:18 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms import IntegerField, TextAreaField, SubmitField
+from wtforms.validators import InputRequired, NumberRange
 
 class FeedbackForm(FlaskForm):
-    rating = IntegerField("Puan (1–5)", validators=[DataRequired(), NumberRange(min=1, max=5)])
-    comment = TextAreaField("Yorum", validators=[DataRequired()])
+    rating = IntegerField("Puan", validators=[InputRequired(), NumberRange(min=1, max=5)])
+    comment = TextAreaField("Yorum")
     submit = SubmitField("Gönder")

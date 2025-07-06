f395c9cdec3eedacbd409e617d20c2bb0e03aeff
KeroRy1 <42krmkrmn07@gmail.com>
2025-07-06 12:19:37 +0300

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, TextAreaField, SubmitField
-from wtforms.validators import InputRequired, NumberRange
-
-class FeedbackForm(FlaskForm):
-    rating = IntegerField("Puan", validators=[InputRequired(), NumberRange(min=1, max=5)])
-    comment = TextAreaField("Yorum")
-    submit = SubmitField("Gönder")
